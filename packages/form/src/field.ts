e72c8ec312df17de180902f5eeb26b4a0f9e16ed
zhangfisher <wxzhang@126.com>
2023-12-20 17:32:15 +0800

    update

--- packages/form/src/field.ts
+++ packages/form/src/field.ts
@@ -8,7 +8,7 @@ import {  FieldComputedProp } from "./types";
 
 export type FPT = Record<string,any> // 字段属性类型集
 
-export interface FieldSchema<PropTypes extends FPT>{
+export interface Field<Value=any,Select=any[]>{
   value        : PropTypes['value'];
   title?       : FieldComputedProp<PropTypes['title']>;                      // 标题
   defaultValue?: FieldComputedProp<PropTypes['value'] | undefined>;          // 默认值
@@ -20,21 +20,35 @@ export interface FieldSchema<PropTypes extends FPT>{
   enable?      : FieldComputedProp<PropTypes['enable']>;                     // 是否可用
   validate?    : FieldComputedProp<PropTypes['validate']>;                   // 验证
   select?      : FieldComputedProp<PropTypes['select']>                      // 枚举值
-
 }
-export type Field<PropTypes extends FPT = {
-  value        : any
-  title?       : string;               // 标题
-  defaultValue?: any;                  // 默认值
-  help?        : string;               // 提示信息
-  placeholder? : string;               // 占位符
-  required?    : boolean;              // 是否必填
-  readonly?    : boolean;              // 是否只读
-  visible?     : boolean;              // 是否可见
-  enable?      : boolean               // 是否可用
-  validate?    : boolean;              // 验证
-  select?      : any[]                 // 枚举值
-}>=FieldSchema<PropTypes>
+// export interface Field<Value=any,Select=any[]>{
+//   value        : PropTypes['value'];
+//   title?       : FieldComputedProp<PropTypes['title']>;                      // 标题
+//   defaultValue?: FieldComputedProp<PropTypes['value'] | undefined>;          // 默认值
+//   help?        : FieldComputedProp<PropTypes['help']>;                       // 提示信息
+//   placeholder? : FieldComputedProp<PropTypes['placeholder']>;                // 占位符
+//   required?    : FieldComputedProp<PropTypes['required']>;                   // 是否必填
+//   readonly?    : FieldComputedProp<PropTypes['readonly']>;                   // 是否只读
+//   visible?     : FieldComputedProp<PropTypes['visible']>;                    // 是否可见
+//   enable?      : FieldComputedProp<PropTypes['enable']>;                     // 是否可用
+//   validate?    : FieldComputedProp<PropTypes['validate']>;                   // 验证
+//   select?      : FieldComputedProp<PropTypes['select']>                      // 枚举值
+// }
+
+
+// export type Field<PropTypes extends FPT = {
+//   value        : any
+//   title?       : string;               // 标题
+//   defaultValue?: any;                  // 默认值
+//   help?        : string;               // 提示信息
+//   placeholder? : string;               // 占位符
+//   required?    : boolean;              // 是否必填
+//   readonly?    : boolean;              // 是否只读
+//   visible?     : boolean;              // 是否可见
+//   enable?      : boolean               // 是否可用
+//   validate?    : boolean;              // 验证
+//   select?      : any[]                 // 枚举值
+// }>=FieldSchema<PropTypes>
 
 
 // 默认同步字段属性
@@ -68,9 +82,7 @@ export type FieldProps<PropTypes extends FPT = FPT> = {
 	name: string 
 	children: FieldRender<PropTypes> | FieldRender<PropTypes>[];
 } 
-  
-// 用来声明表单字段的元数据
-// export type Field<R=any> = R  | ComputedReturns<R> | AsyncComputedReturns<R>;        
+      
 
 export type FieldComponent = React.FC<FieldProps>;
 
