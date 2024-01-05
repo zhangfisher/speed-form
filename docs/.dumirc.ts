b5fa4ac63a3b82a99ec71f6c68da4e372dbb8ce4
zhangfisher <wxzhang@126.com>
2024-01-05 17:53:57 +0800

    fix: docs logo link error

--- docs/.dumirc.ts
+++ docs/.dumirc.ts
@@ -9,12 +9,12 @@ export default defineConfig({
 
     }
   },
-  logo:"logo.png",
+  logo:"/speed-form/logo.png",
   locales: [
     { id: 'zh-CN', name: '中文' },
     { id: 'en-US', name: 'EN' },
   ],
   styles:[
-    "styles.css"
+    "/speed-form/styles.css"
   ]
 });
