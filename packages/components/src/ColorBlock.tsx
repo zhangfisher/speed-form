a61bb714b6e3bb194c33b51ea99f53c99b9e9471
zhangfisher <wxzhang@126.com>
2024-02-29 13:47:24 +0800

    update

--- packages/components/src/ColorBlock.tsx
+++ packages/components/src/ColorBlock.tsx
@@ -30,7 +30,7 @@ export const ColorBlock:ReactFC<React.PropsWithChildren<{value?:any,name?:string
 
   return (
     <div  {...props} style={{  backgroundColor,padding:'4px',color:textColor,display:'flex',...props.style,alignItems:'center' }}>
-      <span style={{flexGrow:1}}>{name ? (<span style={{padding:'2px'}}>{name}=</span>) : ''}<span style={{padding:'2px'}}>{String(value)}</span></span>
+      <span style={{flexGrow:1}}>{name ? (<span style={{padding:'2px'}}>{name}=</span>) : ''}<span style={{padding:'2px'}}>{String(value)}{props.children}</span></span>
       <span style={{fontSize:'8px'}}>{renderCount.current}</span>
     </div>
   );
